3d5de18d08bb1134516054328a855a8d288325c0
Divyanshu Pandey <155071381+CodeCr4cker@users.noreply.github.com>
2025-06-28 10:17:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const syncPointer = ({ x: pointerX, y: pointerY }) => {
+const syncPointer = ({ clientX: pointerX, clientY: pointerY }) => {
   const x = pointerX.toFixed(2);
   const y = pointerY.toFixed(2);
   const xp = (pointerX / window.innerWidth).toFixed(2);
@@ -8,4 +8,23 @@ const syncPointer = ({ x: pointerX, y: pointerY }) => {
   document.documentElement.style.setProperty('--y', y);
   document.documentElement.style.setProperty('--yp', yp);
 };
-document.body.addEventListener('pointermove', syncPointer);
+
+const activateGlow = (e) => {
+  syncPointer(e);
+  document.documentElement.classList.add('clicked');
+  setTimeout(() => {
+    document.documentElement.classList.remove('clicked');
+  }, 500);
+};
+
+document.querySelectorAll('button').forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    activateGlow(e);
+  });
+
+  btn.addEventListener('touchstart', (e) => {
+    if (e.touches.length > 0) {
+      activateGlow(e.touches[0]);
+    }
+  });
+});
